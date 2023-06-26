412c15040e25da4ad44e8afaf007e01ba80dd7f2
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-06-26 20:36:27 +0530

    submitting code-Mon Jun 26 2023 20:36:25 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,24 @@ import React, { useEffect, useState } from 'react';
 import '../styles/App.css'
 import { marked } from 'marked';
 
+
 const App = () => {
     const [text,setText]  = useState('');
     const handleChange = (e) => {
         setText(e.target.value)
     }
     useEffect(()=>{
-        const markdowntext = marked(text);
-        document.getElementById('view').innerHTML = markdowntext;
-    },[text])
+       marked.setOptions({
+        breaks: true,
+       })
+    },[]);
   return (
     <div className='parent'>
       <div className='writing'>
         <textarea type='text' value={text} onChange={handleChange} />
       </div>
       <div className='printing'>
-      <h2 id='view'></h2>
+      <h2 id='view' dangerouslySetInnerHTML={{__html:marked(text)}}></h2>
       </div>
     </div>
   );
